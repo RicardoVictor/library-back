e992ef46678332f8289659e3fa33f66a94453f7e
Ricardo Victor <163028608+RicardoVictorStefanini@users.noreply.github.com>
2024-07-24 20:00:27 -0300

    ordenação

---
+++ Data.Configuration/Migrations/20240724191727_DeleteNoAction.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Configuration.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeleteNoAction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Book_Author_AuthorId",
+                schema: "Library",
+                table: "Book");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Book_Gender_GenderId",
+                schema: "Library",
+                table: "Book");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Book_Author_AuthorId",
+                schema: "Library",
+                table: "Book",
+                column: "AuthorId",
+                principalSchema: "Library",
+                principalTable: "Author",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Book_Gender_GenderId",
+                schema: "Library",
+                table: "Book",
+                column: "GenderId",
+                principalSchema: "Library",
+                principalTable: "Gender",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Book_Author_AuthorId",
+                schema: "Library",
+                table: "Book");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Book_Gender_GenderId",
+                schema: "Library",
+                table: "Book");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Book_Author_AuthorId",
+                schema: "Library",
+                table: "Book",
+                column: "AuthorId",
+                principalSchema: "Library",
+                principalTable: "Author",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Book_Gender_GenderId",
+                schema: "Library",
+                table: "Book",
+                column: "GenderId",
+                principalSchema: "Library",
+                principalTable: "Gender",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
